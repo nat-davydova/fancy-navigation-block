39839fbb0d6486c4899a8111150f2cafd29e3eb5
nat-davydova <natti.davydova@gmail.com>
2022-02-14 17:58:50 +0300

    feat(navigation): make images change on mouseover

--- dist/script.js
+++ dist/script.js
@@ -1,6 +1,7 @@
 const DOM = {
   item: ".fancy-nav__item",
-  itemList: ".fancy-nav__list"
+  itemList: ".fancy-nav__list",
+  images: ".fancy-nav__img"
 };
 const itemList = document.querySelector(DOM.itemList);
 
@@ -11,12 +12,24 @@ function getCurrentItemIndex(item) {
   return currentIndex;
 }
 
+function changeMainImage(currentIndex) {
+  const fancyImgs = document.querySelectorAll(DOM.images);
+
+  fancyImgs.forEach((imgElem, index) => {
+    imgElem.style.opacity = 0;
+
+    if (index === currentIndex) {
+      imgElem.style.opacity = 1;
+    }
+  });
+}
+
 itemList.addEventListener("mouseover", (event) => {
   const target = event.target;
 
   if (target.closest(DOM.item)) {
     const currentFancyItemIndex = getCurrentItemIndex(target.closest(DOM.item));
 
-    console.log(currentFancyItemIndex);
+    changeMainImage(currentFancyItemIndex);
   }
 });
