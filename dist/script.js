92c851f6f93f132f1bfea0b9f25899664c35d10e
nat-davydova <natti.davydova@gmail.com>
2022-02-16 20:35:21 +0300

    feat(tabs): show tab onclick

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,9 @@
 const DOM = {
   item: ".fancy-nav__item",
   itemList: ".fancy-nav__list",
-  images: ".fancy-nav__img"
+  images: ".fancy-nav__img",
+  tabList: ".fancy-nav__tabs",
+  tab: ".fancy-nav__tab"
 };
 const fancyItemList = document.querySelector(DOM.itemList);
 const fancyItems = document.querySelectorAll(DOM.item);
@@ -24,6 +26,15 @@ function changeMainImage(currentIndex) {
   });
 }
 
+function openFancyTab(currentIndex) {
+  const tabList = document.querySelector(DOM.tabList);
+  tabList.classList.add("is-visible");
+
+  const currentTab = document.querySelectorAll(DOM.tab)[currentIndex];
+
+  currentTab.classList.add("is-visible");
+}
+
 fancyItemList.addEventListener("mouseover", (event) => {
   const target = event.target;
 
@@ -31,5 +42,15 @@ fancyItemList.addEventListener("mouseover", (event) => {
     const currentFancyItemIndex = getCurrentItemIndex(target.closest(DOM.item));
 
     changeMainImage(currentFancyItemIndex);
+  }
+});
+
+fancyItemList.addEventListener("click", (event) => {
+  const target = event.target;
+
+  if (target.closest(DOM.item)) {
+    const currentFancyItemIndex = getCurrentItemIndex(target.closest(DOM.item));
+
+    openFancyTab(currentFancyItemIndex);
   }
 });
